ed9454726812abec9cdf3af9f55a008d8805afe2
Amvisarut <visarut.thk@gmail.com>
2025-08-07 10:04:08 +0700

    fix:fix for deploy

--- app/plugins/vue-toastification.client.ts
+++ app/plugins/vue-toastification.client.ts
@@ -1,8 +1,9 @@
-import Toast, * as ToastModule from "vue-toastification";
+const ToastModule = require("vue-toastification");
 import type { PluginOptions } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
-const { POSITION } = ToastModule;
+const { default: Toast, POSITION } = ToastModule;
+
 export default defineNuxtPlugin((nuxtApp) => {
   const options: PluginOptions = {
     position: POSITION.TOP_CENTER,
